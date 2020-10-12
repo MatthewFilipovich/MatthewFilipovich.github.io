be2b74a93ee9dc8c62e747440809d40c9d6ed8d0
Matthew <mjfilipovi@gmail.com>
2020-10-12 18:34:54 -0400

    Optimize buttons for mobile

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,5 @@
-let images = ["images/sanjuan.jpg", "images/sondel.jpg", "images/scotland.jpg", "images/croatia.jpg",
-    "images/georgianbay.jpg", "images/ometepe.jpg", "images/sanfran.jpg", "images/shire.jpg", "images/austria.jpg"];
+let images = ["images/delsur.jpg", "images/sondel.jpg", "images/scotland.jpg", "images/croatia.jpg",
+    "images/georgianbay.jpg", "images/ometepe.jpg", "images/sanfran.jpg", "images/theshire.jpg", "images/austria.jpg"];
 let imageLocations = ["San Juan Del Sur, Nicaragua", "Sondel, Netherlands", "Pentland Hills, Scotland", "Makarska, Croatia",
     "Georgian Bay, Canada", "Ometepe Island, Nicaragua", "San Francisco, United States", "The Shire, Eriador", "Kitzbühel, Austria"];
 var i = Math.floor(Math.random() * images.length);
